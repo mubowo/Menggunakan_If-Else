c75048c25a2793b6dcf55674763ae644927c3865
mubowo <mubowo@gmail.com>
2018-07-11 06:25:41 -0400

    adding .js file

---
+++ JavaScript Conditional (if-else).js
@@ -0,0 +1,28 @@
+
+var nama = "Zero";
+var peran = "Penyihir";
+
+if (nama == "" && peran =="") {
+  console.log("Nama harus diisi!");
+}
+
+else if ( nama === nama && peran == "") {
+  console.log("Halo "+nama+" Pilih peran mu untuk memulai game!");
+}
+
+
+else if ( nama === nama && peran == "Ksatria") {
+  console.log("Selamat datang di Dunia Proxytia, "+nama);
+  console.log("Halo Ksatria "+nama+" kamu dapat menyerang dengan senjatamu!");
+}
+
+else if ( nama === nama && peran == "Tabib") {
+  console.log("Selamat datang di Dunia Proxytia, "+nama);
+  console.log("Halo Tabib "+nama+" kamu akan membantu temanmu yang terluka.");
+}
+
+else if ( nama === nama && peran == "Penyihir") {
+  console.log("Selamat datang di Dunia Proxytia, "+nama);
+  console.log("Halo Penyihir "+nama+" ciptakan keajaiban yang membantu kemenanganmu!");
+}
+
